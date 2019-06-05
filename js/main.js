5b5263139d2ddcd82b63d3836906d7a18cb59b7d
Matan Nadav <Smatannadav@gmail.com>
2019-06-05 15:09:19 +0300

    controller functions part1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 'use strict'
+console.log('online');
 
+let canvas;
+
+let ctx;
+
+let currElement;
 
 
 function init() {
-    
+    canvas = document.querySelector('.canvas');
+    ctx = canvas.getContext('2d');
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerheight;
+}
+
+
+function changeElement(val) {
+    currElement = val;
+}
+
+
+
+function onCanvasClick() {
+
+    const { offsetX, offsetY } = ev;
+    switch (currElement) {
+        case 'triangle':
+            drawTriangle()
+            break;
+        case 'rect':
+            drawRect(offsetX, offsetY)
+            break;
+        case 'text':
+            drawText('test',offsetX, offsetY)
+            break;
+    }
+    ctx.restore()
 }
